86b78ef1608081f6e019bf0c6ec234c6251c8a96
Shrief Essam <shriefessam1999@gmail.com>
2022-02-08 22:00:03 +0200

    final mi card

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,18 +1,100 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Center(child: Text("I Am Rich")),
-          backgroundColor: Colors.blueGrey[900],
-        ),
         backgroundColor: Colors.grey,
-        body: const Center(
-          child: Image(image: AssetImage("images/lana.jpg")),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage("images/me.jpg"),
+                ),
+                Text(
+                  "Shrief Essam",
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Flutter Developer'.toUpperCase(),
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.teal[100],
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 8,
+                  ),
+                ),
+                SizedBox(
+                  width: 200,
+                  height: 20.0,
+                  child: Divider(
+                    color: Colors.teal[100],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(vertical: 6, horizontal: 40),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal[900],
+                      ),
+                      title: Text(
+                        "010 9943 6442",
+                        style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          color: Colors.teal[900],
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(vertical: 0, horizontal: 40),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal[900],
+                      ),
+                      title: Text(
+                        "shriefessam1999@gmail.com",
+                        style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          color: Colors.teal[900],
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
